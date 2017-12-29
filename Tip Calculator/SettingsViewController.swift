cc5d1538f63b6693ae79c8aab635eb1de2f189e8
Harjas Monga <harjasmonga@gmail.com>
2017-12-28 20:24:36 -0600

    updated settings view controller

--- Tip Calculator/SettingsViewController.swift
+++ Tip Calculator/SettingsViewController.swift
@@ -10,18 +10,53 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var useCustomTip: UISwitch!
+    @IBOutlet weak var percentageControl: UISegmentedControl!
+    @IBOutlet weak var checksLabel: UITextField!
+    @IBOutlet weak var customTipLabel: UITextField!
+    
+    let userDefaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
+    override func viewWillAppear(_ animated: Bool) {
+        loadSettings()
+    }
+    // prepopulates settings view with saved settings
+    func loadSettings() {
+        percentageControl.selectedSegmentIndex = userDefaults.integer(forKey: "tipIndex")
+        checksLabel.text = String(userDefaults.integer(forKey: "numOfChecks"))
+        useCustomTip.isOn = userDefaults.bool(forKey: "useCustomTip")
+        customTipLabel.text = String(format: "%.0f", userDefaults.double(forKey: "customTip") * 100)
+    }
+    // makes keyboard disappear
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    //saves new number of checks for the split check calculation
+    @IBAction func numOfCheckChanged(_ sender: Any) {
+        userDefaults.set(Int(checksLabel.text!), forKey: "numOfChecks")
+    }
+    //saves the new custom tip value
+    @IBAction func updateCustomTip(_ sender: Any) {
+        let customTip = (Double(customTipLabel.text!) ?? 0) / 100
+        userDefaults.set(customTip, forKey: "customTip")
+    }
+    //saves the new bool to use the custom tip or not
+    @IBAction func updateCustomTipBoolean(_ sender: Any) {
+        let toUseCustomTip = useCustomTip.isOn
+        userDefaults.set(toUseCustomTip, forKey: "useCustomTip")
+    }
+    //saves the default percentage
+    @IBAction func updateDefaultPercentage(_ sender: Any) {
+        userDefaults.set(percentageControl.selectedSegmentIndex, forKey: "tipIndex")
+        print("tip index default set to: " + String(userDefaults.integer(forKey: "tipIndex")))
+    }
     /*
     // MARK: - Navigation
 
